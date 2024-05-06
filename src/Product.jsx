a64552ee4519cd290916834ff20e4c07bdfd177f
Gloria Villa <hello.gloria@gmail.com>
2024-05-05 18:13:12 -0700

    add useState setName,setPrice

--- src/Product.jsx
+++ src/Product.jsx
@@ -4,18 +4,19 @@ import React, { useState, useEffect } from 'react'
 const Product = () => {
 
   let [name, setName] = useState('Iphone 13');
-
+  let [price, setPrice] = useState(1000);
 
   useEffect(() => {
     console.log('Product Mounted');
-  } , [])
+  }, [])
 
 
   return (
     <div className="product">
       <h1>{name}</h1>
-      <h3>10,000</h3>
+      <h3>{price}</h3>
       <button onClick={() => setName('Samsung S22')}>Change Name</button>
+      <button onClick={() => setPrice('134000')}>Change Price</button>
     </div>
   )
 }
